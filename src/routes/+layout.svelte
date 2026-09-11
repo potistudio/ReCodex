426dd5db670a1f9e372a1c6308d26211d002a12b
potistudio <potistudio@proton.me>
2026-09-11 19:24:18 +0900

    Revert "feat: add CSS Studio development tooling"
    This reverts commit 806f90fbf600e59c836a998ce525e6dd4e21ee55.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
 import "../app.css";
-import { onMount } from "svelte";
 
 let { children } = $props();
-
-onMount(() => {
-	if (!import.meta.env.DEV) return;
-
-	let stopStudio: (() => void) | undefined;
-	void import("cssstudio").then(({ startStudio }) => {
-		stopStudio = startStudio();
-	});
-
-	return () => stopStudio?.();
-});
 </script>
 
 {@render children()}
